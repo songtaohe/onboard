1054fa8c42747e99451f4e09c79b6d410158ef16
Songtao He <songtao@mit.edu>
2018-10-22 09:05:52 +0100

    sync

--- qrcode.py
+++ qrcode.py
@@ -2,13 +2,21 @@ import cv2
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 from time import time 
+import numpy as np
+from sklearn.cluster import MeanShift, estimate_bandwidth
+import sys
 
+
+output_folder = None
+
+if len(sys.argv)>1 :
+    output_folder = sys.argv[1]
 
 cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('image', 960, 720)
 
-dimx = 640
-dimy = 360
+dimx = 720*2
+dimy = 480*2
 
 camera = PiCamera(sensor_mode=5)
 
@@ -18,7 +26,7 @@ camera.resolution = (dimx, dimy)
 
 
 camera.framerate = 30
-camera.shutter_speed = 8000
+#camera.shutter_speed = 8000
 #camera.contrast = 100
 
 #camera.image_effect = ''
@@ -31,7 +39,7 @@ tmp = cv2.imread("target_qrcode/barcode2.png")
 
 target_barcode = cv2.resize(tmp, (96,96))
 
-orb = cv2.ORB_create(nfeatures=100, nlevels = 8, scaleFactor=1.2, patchSize=31, edgeThreshold=15, fastThreshold = 1)
+orb = cv2.ORB_create(nfeatures=100, nlevels = 6, scaleFactor=1.2, patchSize=31, edgeThreshold=15, fastThreshold = 1)
 
 kp_target = orb.detect(target_barcode, None)
 
@@ -52,7 +60,7 @@ for frame in camera.capture_continuous(rawCapture, format = "bgr", use_video_por
     
     t0 = time()
     
-    orb = cv2.ORB_create(nfeatures=800, nlevels = 8, scaleFactor=2.0, patchSize=31, edgeThreshold=15, fastThreshold = 5)
+    orb = cv2.ORB_create(nfeatures=800, nlevels = 6, scaleFactor=2.0, patchSize=31, edgeThreshold=15, fastThreshold = 20)
     
     kp = orb.detect(image, None)
     
@@ -78,15 +86,82 @@ for frame in camera.capture_continuous(rawCapture, format = "bgr", use_video_por
             match_good = matches[:ind]
             break
     
+
+    #X = np.float32([kp[x.queryIdx].pt for x in match_good]).reshape(-1,2)
+    #X = np.float32([x.pt for x in kp]).reshape(-1,2)
+
+    #print(X)
+
+    #bandwidth = estimate_bandwidth(X, quantile=0.1)
+    
+    
+    #print(bandwidth)
+    
+    
+    #ms = MeanShift(bandwidth = 128, bin_seeding=True, cluster_all = False)
+    #ms.fit(X)
+    
+    #labels = ms.labels_
+    #cluster_centers = ms.cluster_centers_
+    
+    #print(labels)
+    #print(cluster_centers)
+    
+    dst = None
+    
+    if len(match_good)>5 :
+    
+        src_pts = np.float32([kp[x.queryIdx].pt for x in match_good]).reshape(-1,1,2)
+        dst_pts = np.float32([kp_target[x.trainIdx].pt for x in match_good]).reshape(-1,1,2)*10
+    
+        
+        #print(dst_pts)
+    
+    
+        M, mask = cv2.findHomography(dst_pts, src_pts, cv2.RANSAC, 2.5)
+    
+        if mask is not None:
+            matchesMask = mask.ravel().tolist()
+            
+            if len(matchesMask) > 5:
+        
+                h,w = target_barcode.shape[0:2]
+                print(h,w)
+                pts = np.float32([[0,0],[0,h-1],[w-1,h-1],[w-1,0]]).reshape(-1,1,2)
+                dst = cv2.perspectiveTransform(pts*10, M)
+    
+                print(M)
+                print(dst)
+    
+   
+        
+    #print(dst)
     
     #print([x.distance for x in matches])
     
     good_kp = [kp[x.queryIdx] for x in match_good]
 
     t2 = time()
-    #img2 = cv2.drawKeypoints(image,good_kp,None,color=(0,255,0), flags=0)
+    #image = cv2.drawKeypoints(image,kp,None,color=(0,255,0), flags=0)
+    
+    img1 = cv2.drawKeypoints(image,good_kp,None,color=(0,255,0), flags=2)
+    
+    #for cls in  cluster_centers:
+    #    img1 = cv2.circle(img1, (int(cls[0]), int(cls[1])), 32, 255,)    
+    
+    if dst is not None:
+        img2 = cv2.polylines(img1, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+    else:
+        img2 = img1
     #img2 = cv2.drawKeypoints(target_barcode,kp_target,None,color=(0,255,0), flags=0)
-    img3 = cv2.drawMatchesKnn(image, kp, target_barcode, kp_target, [match_good], None, flags=2)
+    img3 = cv2.drawMatchesKnn(img2, kp, target_barcode, kp_target, [match_good], None, flags=2)
+    
+    #img3 = cv2.drawMatchesKnn(img2, kp, target_barcode, kp_target, [match_good], None, flags=2)
+    
+    if output_folder is not None :
+        cv2.imwrite(output_folder+"/img%05d.jpg" % c, img3)
+    
+    
     cv2.imshow("image", img3)
     
     t3 = time()
